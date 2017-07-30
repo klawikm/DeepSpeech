9c38f598b1189ff4b46bab33883995b42d403bb0
Ubuntu <bmadmin@deepSpeechPL3.v301uzblwejephwzv2phqdi3ah.jx.internal.cloudapp.net>
2017-07-30 13:37:02 +0000

    Set training parameters in run-polish.sh

--- bin/run-polish.sh
+++ bin/run-polish.sh
@@ -5,19 +5,24 @@ if [ ! -f DeepSpeech.py ]; then
     exit 1
 fi;
 
-if [ ! -f "data/polish/polish.csv" ]; then
+if [ ! -f "data/polish/polish_train.csv" ]; then
     echo "Downloading and preprocessing Polish example data, saving in ./data/polish."
-    python -u bin/import_polish.py ./data/polish polish
+    python -u bin/import_polish.py ./data/polish polish_train
 fi;
 
 python -u DeepSpeech.py \
   --train_files data/polish/polish_train.csv \
   --dev_files data/polish/polish_dev.csv \
   --test_files data/polish/polish_test.csv \
-  --train_batch_size 1 \
-  --dev_batch_size 1 \
-  --test_batch_size 1 \
+  --train_batch_size 20 \
+  --dev_batch_size 10 \
+  --test_batch_size 10 \
   --n_hidden 494 \
-  --epoch 50 \
+  --epoch 2000 \
+  --display_step 10 \
+  --validation_step 10 \
+  --dropout_rate 0.30 \
+  --default_stddev 0.046875 \
+  --learning_rate 0.0001 \
   --checkpoint_dir "~/DeepSpeechPolish" \
   "$@"
